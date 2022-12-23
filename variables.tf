b5bc6e3ac474142e2d461842f9d3843c24abce31
tony@foxmax.onmicrosoft.com <fowlert>
2022-12-23 12:10:45 +0000

    v

--- variables.tf
+++ variables.tf
@@ -1,12 +1,10 @@
 variable "resource_group_name" {
-    default = "variable-rg99"
     type = string
     description = "azure resource group name"
   
 }
 
 variable "resource_group_location" {
-    default = "Uk South"
     type = string
     description = "azure resource group location"   
   
